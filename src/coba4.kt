0e140a5484649a3f5ca81e1cda331c14f10191b2
reinaldoriant <reinaldoriant@gmail.com>
2021-02-18 14:32:27 +0700

    2D Array

--- src/coba4.kt
+++ src/coba4.kt
@@ -1,44 +1,44 @@
-import java.util.*
-
-/*
-question
-There is a string,n , of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first  letters of the infinite string.
-*/
-
-/*// Complete the repeatedString function below.
-fun repeatedString(s: String, n: Long): Long {
-    val numOfS: Long = n / s.length
-    val rest: Long = n % s.length
-
-    if (!s.contains("a")) return 0
-    return if (s.length > n) aCounter(s, rest) else numOfS * aCounter(s, s.length.toLong()) + aCounter(s, rest)
-
-}
-private fun aCounter(s: String, end: Long): Long {
-    var a = 0
-    for (i in 0 until end) {
-        if (s[i.toInt()] == 'a') a++
-    }
-    return a.toLong()
-}*/
-
-fun repeatedString(s: String, n: Long): Long {
-    val fullRepeats = n.div(s.length)
-    val leftOver = (n % s.length).toInt()
-
-    val perString = s.count { it == 'a' }
-    val remainder = s.take(leftOver).count { it == 'a' }
-    return fullRepeats.times(perString).plus(remainder)
-}
-
-fun main(args: Array<String>) {
-    val scan = Scanner(System.`in`)
-
-    val s = scan.nextLine()
-
-    val n = scan.nextLine().trim().toLong()
-
-    val result = repeatedString(s, n)
-
-    println(result)
-}
+//import java.util.*
+//
+///*
+//question
+//There is a string,n , of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first  letters of the infinite string.
+//*/
+//
+///*// Complete the repeatedString function below.
+//fun repeatedString(s: String, n: Long): Long {
+//    val numOfS: Long = n / s.length
+//    val rest: Long = n % s.length
+//
+//    if (!s.contains("a")) return 0
+//    return if (s.length > n) aCounter(s, rest) else numOfS * aCounter(s, s.length.toLong()) + aCounter(s, rest)
+//
+//}
+//private fun aCounter(s: String, end: Long): Long {
+//    var a = 0
+//    for (i in 0 until end) {
+//        if (s[i.toInt()] == 'a') a++
+//    }
+//    return a.toLong()
+//}*/
+//
+//fun repeatedString(s: String, n: Long): Long {
+//    val fullRepeats = n.div(s.length)
+//    val leftOver = (n % s.length).toInt()
+//
+//    val perString = s.count { it == 'a' }
+//    val remainder = s.take(leftOver).count { it == 'a' }
+//    return fullRepeats.times(perString).plus(remainder)
+//}
+//
+//fun main(args: Array<String>) {
+//    val scan = Scanner(System.`in`)
+//
+//    val s = scan.nextLine()
+//
+//    val n = scan.nextLine().trim().toLong()
+//
+//    val result = repeatedString(s, n)
+//
+//    println(result)
+//}
---
+++ src/coba5.kt
@@ -0,0 +1,43 @@
+//import java.util.*
+//
+//Given a 6x6  2D Array, Calculate the hourglass sum for every hourglass in arr , then print the maximum hourglass sum. The array will always be 6x6
+
+//// Complete the hourglassSum function below.
+//fun hourglassSum(arr: Array<Array<Int>>): Int {
+//    var highest = 0
+//    var first = true
+//    for (i in 0..3){
+//        for (r in 0..3){
+//            val firstRow = arr[i][r] + arr[i][r+1] + arr[i][r+2]
+//            val secondRow = arr[i+1][r+1]
+//            val thirdRow = arr[i+2][r] + arr[i+2][r+1] + arr[i+2][r+2]
+//            val totalValue = firstRow + secondRow + thirdRow
+//            if (first){
+//                highest = totalValue
+//                first = false
+//            }
+//            else {
+//                if (totalValue > highest){
+//                    highest = totalValue
+//                }
+//            }
+//        }
+//
+//
+//    }
+//    return highest
+//}
+//
+//fun main(args: Array<String>) {
+//    val scan = Scanner(System.`in`)
+//
+//    val arr = Array<Array<Int>>(6, { Array<Int>(6, { 0 }) })
+//
+//    for (i in 0 until 6) {
+//        arr[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+//    }
+//
+//    val result = hourglassSum(arr)
+//
+//    println(result)
+//}
